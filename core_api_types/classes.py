149926bcfd5ac479188b9aaa87ee02939d775163
Nicholas Foreman <nicholas.w.foreman@outlook.com>
2020-12-29 18:24:33 -0600

    Base Off API from Two Months Ago for Changes

--- core_api_types/classes.py
+++ core_api_types/classes.py
@@ -16,9 +16,9 @@ def RemoveStaticFunction(differences: list, returnType: str, className: str, fun
 	differences.append("\tRemoved " + "StaticFunction " + returnType + " " + className + "." + functionName + parameterFormat)
 
 def AddMemberFunction(differences: list, returnType: str, className: str, functionName: str, parameterFormat: str):
-	differences.append("\tAdded " + "MemberFunction " + returnType + " " + className + "." + functionName + parameterFormat)
+	differences.append("\tAdded " + "MemberFunction " + returnType + " " + className + ":" + functionName + parameterFormat)
 def RemoveMemberFunction(differences: list, returnType: str, className: str, functionName: str, parameterFormat: str):
-	differences.append("\tRemoved " + "MemberFunction " + returnType + " " + className + "." + functionName + parameterFormat)
+	differences.append("\tRemoved " + "MemberFunction " + returnType + " " + className + ":" + functionName + parameterFormat)
 
 def GetClassByName(classes: list, className: str):
 	for _class in classes["Classes"]:
@@ -34,39 +34,39 @@ def GetFunctionByName(_class: dict, functionName: str, _type: str):
 			return function
 
 
-def AddSignature(differences, signature, className: str, functionName: str, isMember: bool, addedOrRemoved: bool):
+def AddSignature(differences, signature, className: str, functionName: str, isMemberFunction: bool, addedOrRemoved: bool):
 	parameterFormat = core_functions.GetParameterFormat(signature)
 	returnType = core_functions.GetReturnType(signature["Returns"])
 	
 	if addedOrRemoved:
-		if isMember:
+		if isMemberFunction:
 			AddMemberFunction(differences, returnType, className, functionName, parameterFormat)
 		else:
 			AddStaticFunction(differences, returnType, className, functionName, parameterFormat)
 	else:
-		if isMember:
+		if isMemberFunction:
 			RemoveMemberFunction(differences, returnType, className, functionName, parameterFormat)
 		else:
 			RemoveStaticFunction(differences, returnType, className, functionName, parameterFormat)
 
 
-def AddAllSignatures(isMember: bool, differences: list, signatures: dict, className: str, functionName: str, addedOrRemoved: bool):
+def AddAllSignatures(isMemberFunction: bool, differences: list, signatures: dict, className: str, functionName: str, addedOrRemoved: bool):
 	for signature in signatures:
-		AddSignature(differences, signature, className, functionName, isMember, addedOrRemoved)
+		AddSignature(differences, signature, className, functionName, isMemberFunction, addedOrRemoved)
 
 
 def AddAllStaticFunctions(differences: list, _class: dict, addedOrRemoved: bool):
 	if "StaticFunctions" not in _class:
 		return
 	for staticFunction in _class["StaticFunctions"]:
-		AddAllSignatures(True, differences, staticFunction["Signatures"], _class["Name"], staticFunction["Name"], addedOrRemoved)
+		AddAllSignatures(False, differences, staticFunction["Signatures"], _class["Name"], staticFunction["Name"], addedOrRemoved)
 
 
 def AddAllMemberFunctions(differences: list, _class: dict, addedOrRemoved: bool):
 	if "MemberFunctions" not in _class:
 		return
 	for staticFunction in _class["MemberFunctions"]:
-		AddAllSignatures(False, differences, staticFunction["Signatures"], _class["Name"], staticFunction["Name"], addedOrRemoved)
+		AddAllSignatures(True, differences, staticFunction["Signatures"], _class["Name"], staticFunction["Name"], addedOrRemoved)
 
 
 def AddAllClassDifferences(differences: list, _class: dict, addedOrRemoved: bool):
@@ -79,10 +79,10 @@ def AddAllClassDifferences(differences: list, _class: dict, addedOrRemoved: bool
 	AddAllMemberFunctions(differences, _class, addedOrRemoved)
 
 
-def CompareFunctions(differences: list, functionList: list, _class: dict, otherClass: dict, isMember: bool, addedOrRemoved: bool, wasChanged: bool):
+def CompareFunctions(differences: list, functionList: list, _class: dict, otherClass: dict, isMemberFunction: bool, addedOrRemoved: bool, wasChanged: bool):
 	for function in functionList:
 		otherFunction = None
-		if isMember:
+		if isMemberFunction:
 			otherFunction = GetFunctionByName(otherClass, function["Name"], "MemberFunctions")
 		else:
 			otherFunction = GetFunctionByName(otherClass, function["Name"], "StaticFunctions")
@@ -96,13 +96,13 @@ def CompareFunctions(differences: list, functionList: list, _class: dict, otherC
 					wasChanged = True
 					ChangeClass(differences, _class["Name"])
 				
-				AddSignature(differences, signature, _class["Name"], function["Name"], isMember, addedOrRemoved)
+				AddSignature(differences, signature, _class["Name"], function["Name"], isMemberFunction, addedOrRemoved)
 		else:
 			if not wasChanged:
 				wasChanged = True
 				ChangeClass(differences, _class["Name"])
 			
-			AddAllSignatures(False, differences, function["Signatures"], _class["Name"], function["Name"], addedOrRemoved)
+			AddAllSignatures(isMemberFunction, differences, function["Signatures"], _class["Name"], function["Name"], addedOrRemoved)
 	
 	return wasChanged
 
